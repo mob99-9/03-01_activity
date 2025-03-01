faa753531a2035e8e992b3e20a37732aa1c50e36
mob99-9 <jamesorozco20050@gmail.com>
2025-03-01 12:22:00 +0800

    Add files via upload

---
+++ prog06.py
@@ -0,0 +1,5 @@
+#Prog06: Create a program that ask the user to input 2 numbers. Print the result when the first number is raised to the second number
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+result = num1**num2
+print(result)
