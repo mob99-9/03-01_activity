ad25c8359585376d73de53239594cbe34a5958d4
mob99-9 <jamesorozco20050@gmail.com>
2025-03-01 11:57:44 +0800

    Add files via upload

---
+++ prog04.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+product = num1 * num2
+print(product)
