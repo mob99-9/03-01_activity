7bcc6ba4103db5b8466c37e99715785f4f29ca5d
mob99-9 <jamesorozco20050@gmail.com>
2025-03-01 12:06:39 +0800

    Add existing file

---
+++ prog03.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+sum = num1 + num2 
+print(sum)
