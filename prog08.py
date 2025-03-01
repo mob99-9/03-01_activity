848ef0d6d56269e4c7aa9d42e819b05abe11092b
mob99-9 <jamesorozco20050@gmail.com>
2025-03-01 12:01:07 +0800

    Delete prog08.py

--- prog08.py
+++
@@ -1,9 +0,0 @@
-#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
-odd = []
-for i in range(0,10):
-    num = float(input(f'Enter number {i + 1}: '))
-    if num % 2 != 0:
-        odd.append(num)
-    else:
-        None
-print(len(odd))
