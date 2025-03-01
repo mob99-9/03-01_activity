5389f85bc75ee3183a09005c440fce9c2019919f
mob99-9 <jamesorozco20050@gmail.com>
2025-03-01 12:00:04 +0800

    Delete prog10.py

--- prog10.py
+++
@@ -1,7 +0,0 @@
-#Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
-tens = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-for i in range(0,101):
-    if i in tens:
-        continue
-    else:
-        print(i)
